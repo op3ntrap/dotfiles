78e14dc9064fe1972677b070714bf159a926bed3
op3ntrap <op3ntrap@gmail.com>
2018-09-19 08:55:35 +0530

    log Update

--- fzf/.fzf.bash
+++ fzf/.fzf.bash
@@ -1,14 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/$USER/.fzf/bin"
+if [[ ! "$PATH" == */home/op3ntrp/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/op3ntrp/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/$USER/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/op3ntrp/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/$USER/.fzf/shell/key-bindings.bash"
+source "/home/op3ntrp/.fzf/shell/key-bindings.bash"
 
